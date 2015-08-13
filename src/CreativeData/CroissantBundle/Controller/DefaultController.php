f540e51e9245b44991297a28aef804dcde759632
Kevin Vézier <kevin@creativedata.fr>
2015-08-13 16:52:30 +0200

    fix use statement

--- src/CreativeData/CroissantBundle/Controller/DefaultController.php
+++ src/CreativeData/CroissantBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use \DateTime;
 use CreativeData\CroissantBundle\Form\HistoryType;
+use CreativeData\CroissantBundle\Entiry\History;
 
 class DefaultController extends Controller {
 
